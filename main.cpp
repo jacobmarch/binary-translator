3544cee809ba4f16e2b911bd43c9efdca2d61e74
Jacob Marchywka <dcfalcons2021@gmail.com>
2022-05-07 14:18:09 -0400

    Completed convertToBinary function and implemented it into main

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ void convertToBinary(int input){
     char arr[8];
 
     
-    if (input < 255){
+    if (input <= 255 && input >= 0){
         for (i = 0; i < 8; i++){
             if (input % 2 == 1){
                 arr[7-i] = '1';
@@ -17,20 +17,38 @@ void convertToBinary(int input){
                 arr[7-i] = '0';
             }
             input = input / 2;
+        }
+        for (i = 0; i < 8; i++){
             cout << arr[i];
         }
-
+    }
+    else if (input == -1){
+        cout << "Quitting...";
+    }
+    else if (input < 0) {
+        cout << "ERROR: Number too small";
     }
     else {
-        cout << "Number too large";
+        cout << "ERROR: Number too large";
     }
+    cout << endl;
 }
 
 int main(){
 
-cin >> userInput;
-convertToBinary(userInput);
 
+
+while (userInput != -1){
+
+    cout << "Enter a number to convert to binary or -1 to quit: ";
+
+    cin >> userInput;
+
+
+    convertToBinary(userInput);
+
+
+}
 return 0;
 
 
